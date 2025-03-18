f4f6811b2ce422a75cdd68f4ba17c99cd6831674
celakk <49827604+celakk1@users.noreply.github.com>
2025-03-18 19:02:35 +0100

    Update clkmenu.lua

--- script/clkmenu.lua
+++ script/clkmenu.lua
@@ -1,4 +1,5 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Sense = loadstring(game:HttpGet('https://sirius.menu/sense'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "clkmenu",
@@ -32,4 +33,91 @@ local Window = Rayfield:CreateWindow({
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
+})
+
+local Tab = Window:CreateTab("main", "rewind")
+local Section = Tab:CreateSection("movement")
+local Toggle = Tab:CreateToggle({
+	Name = ApplyUnsupportedName("autosprint", Success),
+	CurrentValue = false,
+	Flag = "sprint",
+	Looped = true,
+	Callback = function()
+		if not Success then
+			return
+		end
+
+		local Character = Player.Character
+
+		if not Character then
+			return
+		end
+
+		local Humanoid: Humanoid = Character:FindFirstChild("Humanoid")
+
+		if not Humanoid then
+			return
+		end
+
+		if Humanoid.MoveDirection == Vector3.zero then
+			return
+		end
+		
+		if Character:FindFirstChild("ServerRun") then
+			return
+		end
+
+		Network.connect("Sprint", "Fire", Character, true)
+	end,
+})
+local Toggle = Tab:CreateToggle({
+	Name = ApplyUnsupportedName("remove fall damage", Success),
+	CurrentValue = false,
+	Flag = "falldmg",
+	Callback = function(Value)
+		if not Success then
+			return
+		end
+
+		if Value then
+			Original = Network.connect
+			Network.connect = function(RemoteName, Method, Character, Settings, ...)
+				if Settings and typeof(Settings) == "table" and Settings.Config == "FallDamage" then
+					return
+				end
+
+				return Original(RemoteName, Method, Character, Settings, ...)
+			end
+		elseif Original then
+			Network.connect = Original
+		end
+	end,
+})
+
+local Toggle = Tab:CreateToggle({
+	Name = "no fog",
+	CurrentValue = false,
+	Flag = "fog",
+	Callback = function(Value)
+		if Value then
+			FogEnd = Lighting.FogEnd
+
+			Lighting.FogEnd = 100000
+
+			for _,v in Lighting:GetDescendants() do
+				if v:IsA("Atmosphere") then
+					table.insert(FogObjects, v)
+					v.Parent = nil
+				end
+			end
+		elseif FogEnd then
+			Lighting.FogEnd = FogEnd
+
+			for _,v in FogObjects do
+				v.Parent = Lighting
+			end
+
+			FogObjects = {}
+		end
+	end,
 })
